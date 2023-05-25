1af383e4d40b7f961f02c484e24240cd5bbe32a1
Harrison Davidson <125293541+HarrisonDavidson@users.noreply.github.com>
2023-05-25 15:50:33 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ Adjust your window to 78 W x 28 L
 
 AskQuestion("Do you hate me?", "Yay!", "You should try it!");
 AskQuestion("Are you afraid of me?", "Don't be scared, dear.", "You will be...oh, yes, you will be...");
-AskQuestion("Do you want to know a secret?", "If you want more, you'll have to ignore. (Ln 400)", "Secrets are the best, I love to share them!");
+AskQuestion("Do you want to know a secret?", "If you want more, you'll have to ignore. (400)", "Secrets are the best, I love to share them!");
 
 
 void MooseSays(string message)

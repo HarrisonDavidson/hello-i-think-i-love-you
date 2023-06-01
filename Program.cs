da2dc48070fae4b01c1cc8a543f0d1c8fee15942
Harrison Davidson <125293541+HarrisonDavidson@users.noreply.github.com>
2023-06-01 08:50:29 -0400

    ???

--- Program.cs
+++ Program.cs
@@ -9,10 +9,9 @@ Console.WriteLine("______________________________________________");
 Console.WriteLine();
 Console.WriteLine();
 
-
 GameSays("H E L L O.  I  T H I N K  I  L O V E  Y O U.");
 
-AskQuestion("Continue?", "H E L L O.  I  T H I N K  I  L O V E  Y O U.", "H E L L O.  I  T H I N K  I  L O V E  Y O U." );
+AskQuestion("Continue?", "H E L L O.  I  T H I N K  I  L O V E  Y O U.", "secrets here, secrets there, secrets everywhere..." );
 AskQuestion("Do you love me?", "Really? It seems very unlikely.", "I KNEW IT !!!");
 }
 
